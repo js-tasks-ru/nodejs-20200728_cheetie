c349142132741925488c08b5eacb076ceffdfec2
Nomta <nomta@mail.ru>
2020-08-07 13:07:00 +0300

    add solution for 3-module 2-task

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,9 +4,17 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this._options = options;
   }
 
   _transform(chunk, encoding, callback) {
+    const lines = chunk
+      .toString(this._options.encoding)
+      .split(os.EOL);
+      
+    for (let line of lines) {
+      this.push(line);
+    }
   }
 
   _flush(callback) {
