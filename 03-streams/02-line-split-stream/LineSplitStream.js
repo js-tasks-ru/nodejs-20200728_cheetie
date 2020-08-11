db7ae537d00b92d45bf88c787b1ec8532d4c3f6c
Nomta <nomta@mail.ru>
2020-08-11 15:39:40 +0300

    add solution for 3-module 2-task

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -5,19 +5,33 @@ class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
     this._options = options;
+    this._cache = '';
   }
 
   _transform(chunk, encoding, callback) {
     const lines = chunk
       .toString(this._options.encoding)
       .split(os.EOL);
+
+    const lastIndex = lines.length - 1;
+    const truncatedLines = lines.slice(1, lastIndex);
+
+    this._cache += lines[0];
+
+    if (lastIndex) {
+      this.push(this._cache);
+      this._cache = lines[lastIndex];
       
-    for (let line of lines) {
-      this.push(line);
+      for (let line of truncatedLines) {
+        this.push(line);
+      }
     }
+    callback();
   }
 
   _flush(callback) {
+    this.push(this._cache);
+    callback();
   }
 }
 
