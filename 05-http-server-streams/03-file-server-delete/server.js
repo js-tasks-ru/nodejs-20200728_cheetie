ca3c0ff0517454473505a226061ab68d7c7f3a3f
Nomta <nomta@mail.ru>
2020-08-13 20:44:24 +0300

    add solution for 5-module 3-task

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,17 +1,32 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const url  = require('url');
+const fs   = require ('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
+      if (pathname.includes('/') || pathname.includes(path.sep)) {
+        res.statusCode = 400;
+        res.end();
+      }
 
+      fs.unlink(filepath, err => {
+        if (err) {
+          if (err.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('not found');
+          } else {
+            res.statusCode = 500;
+            res.end('internal server error');
+          }
+        }
+      })
       break;
 
     default:
