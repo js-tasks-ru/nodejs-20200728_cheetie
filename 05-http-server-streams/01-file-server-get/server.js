82e10001d93068bf055e9d33c317e5592b816778
Nomta <nomta@mail.ru>
2020-08-13 21:01:24 +0300

    add solution for 5-module 1-task

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -14,6 +14,7 @@ server.on('request', (req, res) => {
       if (pathname.includes('/') || pathname.includes(path.sep)) {
         res.statusCode = 400;
         res.end();
+        return;
       }
 
       fs
