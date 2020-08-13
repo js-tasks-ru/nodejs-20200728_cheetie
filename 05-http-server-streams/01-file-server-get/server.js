1a558ba12d1c28e23707e9d9e6a53502595ab9f4
Nomta <nomta@mail.ru>
2020-08-13 13:40:53 +0300

    add solution for 5-module 3-task

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,17 +1,33 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const url  = require('url');
+const fs   = require ('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+
+  if (pathname.includes('/') || pathname.includes(path.sep)) {
+    res.statusCode = 400;
+    res.end();
+  }
 
   switch (req.method) {
     case 'GET':
-
+      fs
+        .createReadStream(filepath)
+        .on('error', err => {
+          if (err.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('not found');
+          } else {
+            res.statusCode = 500;
+            res.end('internal server error');
+          }
+        })
+        .pipe(res);
       break;
 
     default:
