16fe06a72501b09ed642ada8c33f4ac4158291c3
Nomta <nomta@mail.ru>
2020-08-15 21:58:42 +0300

    add solution for 5-module 2-task

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -8,15 +8,52 @@ const server = new http.Server();
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
   const filepath = path.join(__dirname, 'files', pathname);
+  const LimitSizeStream = require('./LimitSizeStream');
+  const limitStream = new LimitSizeStream({limit: 1024});
 
-  if (pathname.includes('/') || pathname.includes(path.sep)) {
-    res.statusCode = 400;
-    res.end();
-  }
+  limitStream.on('error', err => {
+    if (err.name === 'LimitExceededError') {
+      res.statusCode = 413;
+      res.end();
+    } else {
+      res.statusCode = 500;
+      res.end();
+    }
+  });
 
   switch (req.method) {
     case 'POST':
+      if (pathname.includes('/') || pathname.includes(path.sep)) {
+        res.statusCode = 400;
+        res.end();
+        return;
+      }
 
+      fs.exists(filepath, exists => {
+        if (exists) {
+          res.statusCode = 409;
+          res.end();
+          return;
+        }
+
+        const writeStream = fs.createWriteStream(filepath, err => {
+          if (err) {
+            res.statusCode = 500;
+            res.end('Server Error');
+          }
+        });
+        
+        writeStream.on('error', () => {
+          fs.unlink(filepath, err => {
+            if (err) {
+              res.statusCode = 500;
+              res.end('Server Error');
+            }
+          });
+        });
+
+        req.pipe(limitStream).pipe(writeStream);
+      });
       break;
 
     default:
