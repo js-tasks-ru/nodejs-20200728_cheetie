542dfb898c69bd61975710c7f842df1b7a3d55c1
Nomta <nomta@mail.ru>
2020-08-22 15:10:22 +0300

    add solution for 5-module 2-task

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -9,39 +9,24 @@ server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
   const filepath = path.join(__dirname, 'files', pathname);
   const LimitSizeStream = require('./LimitSizeStream');
-  const limitStream = new LimitSizeStream({limit: 1024});
-
-  limitStream.on('error', err => {
-    if (err.name === 'LimitExceededError') {
-      res.statusCode = 413;
-      res.end();
-    } else {
-      res.statusCode = 500;
-      res.end();
-    }
-  });
+  const limitStream = new LimitSizeStream({limit: 1048576});
 
   switch (req.method) {
     case 'POST':
       if (pathname.includes('/') || pathname.includes(path.sep)) {
         res.statusCode = 400;
-        res.end();
+        res.end('Server Error');
         return;
       }
 
       fs.exists(filepath, exists => {
         if (exists) {
           res.statusCode = 409;
-          res.end();
+          res.end('File exists');
           return;
         }
 
-        const writeStream = fs.createWriteStream(filepath, err => {
-          if (err) {
-            res.statusCode = 500;
-            res.end('Server Error');
-          }
-        });
+        const writeStream = fs.createWriteStream(filepath);
         
         writeStream.on('error', () => {
           fs.unlink(filepath, err => {
@@ -50,6 +35,32 @@ server.on('request', (req, res) => {
               res.end('Server Error');
             }
           });
+        });
+        
+        writeStream.on('finish', () => {
+          res.statusCode = 201;
+          res.end('Success');
+        });
+        
+        res.on('close', () => {
+          if (res.finished) {
+            return;
+          }
+          res.statusCode = 500;
+          res.end('Server Error');
+        });
+
+        limitStream.on('error', err => {
+          if (err) {
+            fs.unlink(filepath, err => {});
+            if (err.name === 'LimitExceededError') {
+              res.statusCode = 413;
+              res.end('Server Error');
+            } else {
+              res.statusCode = 500;
+              res.end('Server Error');
+            }
+          }
         });
 
         req.pipe(limitStream).pipe(writeStream);
