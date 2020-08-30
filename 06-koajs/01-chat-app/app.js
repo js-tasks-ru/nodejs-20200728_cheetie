c331910547bcf36b52833884eeeb8256070323a4
Nomta <nomta@mail.ru>
2020-08-30 20:07:33 +0300

    add solution for 6-module 1-task

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -8,10 +8,35 @@ app.use(require('koa-bodyparser')());
 const Router = require('koa-router');
 const router = new Router();
 
+const messages = [];
+const clients = [];
+
+const getMessages = () => {
+    return new Promise(resolve => {
+        if (messages.length) resolve(messages);
+        else setTimeout(() => resolve(getMessages()), 500);
+    });
+}
+
 router.get('/subscribe', async (ctx, next) => {
+    const message = await getMessages()
+        .then(messages => messages.join('\n'));
+
+    ctx.request.body = message;
+    messages.length = 0;
+
+    await next();
+}, async (ctx, next) => {
+    ctx.response.status = 200;
+    ctx.body = ctx.request.body;
 });
 
 router.post('/publish', async (ctx, next) => {
+    const { message } = ctx.request.body;
+    if (!message) return ctx.throw(400, 'message is required');
+    messages.push(message);
+    ctx.response.status = 201;
+    ctx.body = "success";
 });
 
 app.use(router.routes());
