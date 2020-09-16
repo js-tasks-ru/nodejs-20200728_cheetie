3dafcf909223b460d530b61d3a486549afb78186
Nomta <nomta@mail.ru>
2020-09-16 08:42:06 +0300

    add solution for 8-module

--- 08-authentication/02-oauth/libs/strategies/authenticate.js
+++ 08-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,22 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');  // npm run test:local 08-authentication 02-oauth
+
+module.exports = async function authenticate(strategy, email, displayName = email, done) {
+  if (!email) {
+    return done(null, false, 'Не указан email');
+  }
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      user = await User.create({ email, displayName });
+      await user.save();
+    }
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
 };
+
+
