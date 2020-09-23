8ed51e7e5514803f6b4e464adbcc01323a1c1942
Nomta <nomta@mail.ru>
2020-09-24 00:28:29 +0300

    add solution for 9-module

--- 09-email/01-registration/controllers/registration.js
+++ 09-email/01-registration/controllers/registration.js
@@ -1,11 +1,52 @@
 const { v4: uuid } = require('uuid');
 const User = require('../models/User');
-const sendMail = require('../libs/sendMail');
+const Session = require('../models/Session');
+const sendMail = require('../libs/sendMail');    
 
 module.exports.register = async (ctx, next) => {
+  const errors = {};
+  const { email, displayName, password } = ctx.request.body;
+  const userExists = await User.findOne({ email });
+  
+  // if (userExists) {
+  //   errors.email = 'Такой email уже существует';
+  //   ctx.throw(400, { errors });
+  // }
+  // try {
+    const verificationToken = uuid();
+    const user = await User.create({ email, displayName, verificationToken });
+    await user.setPassword(password);
+    await user.save();
+    
+    await sendMail({
+        template: 'confirmation',
+        locals: {token: verificationToken},
+        to: email,
+        subject: 'Подтвердите почту'
+    });
 
+    ctx.body = { status: 'ok' };
+    
+  // } catch(err) {
+  //   if (err.name !== 'ValidationError') throw err;
+  //   throw new ValidationError('Некорректный email.');
+  // }
 };
 
 module.exports.confirm = async (ctx, next) => {
-  
+    const verificationToken = ctx.request.body.verificationToken;
+    const user = await User.findOne({ verificationToken });
+
+    if (!user) {
+      ctx.throw(400, 'Ссылка подтверждения недействительна или устарела');
+    }
+    
+    user.verificationToken = undefined;
+    await user.save();
+
+    const token = uuid();
+    const session = new Session({ token, user: user.id, lastVisit: new Date() });
+    await session.save();
+
+    ctx.body = { token };
 };
--- 09-email/02-order/controllers/orders.js
+++ 09-email/02-order/controllers/orders.js
@@ -1,8 +1,41 @@
 const Order = require('../models/Order');
+const mapOrder = require('../mappers/order');
+const mapProduct = require('../mappers/product');
 const sendMail = require('../libs/sendMail');
 
 module.exports.checkout = async function checkout(ctx, next) {
+    const user = ctx.user;
+    const { product, phone, address } = ctx.request.body;
+
+    if (!user) {
+        ctx.throw(401, 'Пользователь не авторизован');
+    }
+    
+    const order = await new Order({
+        user: user.id, product, phone, address
+    });
+    await order.save();
+    
+    await sendMail({
+        template: 'order-confirmation',
+        locals: { 
+            id: order.id, 
+            product: mapProduct(order.product) 
+        },
+        to: user.email,
+        subject: 'Подтверждение заказа'
+    });
+
+    ctx.body = { status: 'ok', order: order.id };
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+    if (!ctx.user) {
+        ctx.throw(401, 'Пользователь не авторизован');
+    }
+
+    const orders = await Order
+        .find({ user: ctx.user.id })
+        // .map(order => mapOrder(order.populate('product')));
+    ctx.body = { orders };
 };
